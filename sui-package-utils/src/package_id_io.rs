4327e7f506438afb0b264f61d9ee007f3e553ac7
Reginaldo Silva <reginaldo.silva@mystenlabs.com>
2025-12-18 18:07:30 -0300

    Get compiled modules from bcs.json

--- sui-package-utils/src/package_id_io.rs
+++ sui-package-utils/src/package_id_io.rs
@@ -1,11 +1,24 @@
+use crate::bcs_json::BcsJsonSchema;
 use crate::metadata::PackageMetadata;
+
+use base64::prelude::*;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::Error as IOError;
 use std::path::PathBuf;
+use std::collections::BTreeMap;
 
+
+use move_binary_format::file_format::CompiledModule;
 pub struct PackagesDir {
     prefix: PathBuf,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageIoError {
+    message: String,
 }
 
 const LAST_62_REGEX: &str = r"/[0-9a-f]{62}$";
@@ -53,6 +66,18 @@ impl PackagesDir {
             }
         }
         Ok(package_directories)
+    }
+
+    pub fn load_package_modules(self: &PackagesDir, id: &str) -> Result<BTreeMap<String, CompiledModule>, PackageIoError> {
+        let package_dir = self.get_package_dir(id);
+        let bcs_json = fs::read_to_string(format!("{}/bcs.json", package_dir)).map_err(|e| PackageIoError { message: e.to_string() })?;
+        let bcs_json: BcsJsonSchema = serde_json::from_str(&bcs_json).map_err(|e| PackageIoError { message: e.to_string() })?;
+        let mut modules = BTreeMap::new();
+        for (module_name, module_b64) in bcs_json.get_module_map() {
+            let module = CompiledModule::deserialize_with_defaults(BASE64_STANDARD.decode(module_b64).map_err(|e| PackageIoError { message: e.to_string() })?.as_slice()).map_err(|e| PackageIoError { message: e.to_string() })?;
+            modules.insert(module_name.clone(), module);
+        }
+        Ok(modules)
     }
 
     pub fn get_latest_checkpoint(self: &PackagesDir) -> Result<u64, IOError> {
